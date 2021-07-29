1c70aedb7750cbe9dcedd033f836b2dfa1966c87
yakhub <sahjarkhan7860@gmail.com>
2021-07-29 15:15:47 +0530

    my first MERN project mdfy2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
-const bcrypt = require("bcryptjs"); 
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const auth = require("./middleware/auth");
 
 require("./db/conn");
 const Register = require("./models/registers");
@@ -21,43 +23,80 @@ const partials_path = path.join(__dirname, "../templates/partials");
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
- 
+
 //------------dotenv-----------------
 
 // console.log(process.env.SECRET_KEY);
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.render("index")
 });
+app.get("/secret", auth, (req, res) => {
+    // console.log(`this is the cookie : ${req.cookies.jwt}`);
+    res.render("secret");
+})
 
-app.get("/register", (req,res) => {
+app.get("/register", (req, res) => {
     res.render("register");
 })
 
-app.get("/login", (req,res) => {
+app.get("/login", (req, res) => {
     res.render("login");
+})
+
+app.get("/logout", auth, async (req, res) => {
+
+    try {
+        console.log(req.user);
+
+
+        //---------for single logout------------
+        // req.user.tokens = req.user.tokens.filter((currElement) => {
+        //     return currElement.token !== req.token
+        // })
+
+
+        //-----------logout for all devices--------
+
+        req.user.tokens = [];
+
+
+        res.clearCookie("jwt");
+        console.log("logout successfully");
+        await req.user.save();
+        res.render("login");
+    } catch (error) {
+        res.status(500).send(error);
+    }
 })
 
 // ---------------------Registration Form----------------------
 
-app.post("/register", async (req,res) => {
+app.post("/register", async (req, res) => {
     try {
 
-       const registerEmployee = new Register({
-           Username: req.body.Username,
-           password:req.body.password
-       })
+        const registerEmployee = new Register({
+            Username: req.body.Username,
+            password: req.body.password
+        })
 
-       const token = await registerEmployee.generateAuthToken();
+        const token = await registerEmployee.generateAuthToken();
 
-       const registered = await registerEmployee.save();
-       res.status(201).render("index")
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 30000),
+            httpOnly: true
+        });
+
+        const registered = await registerEmployee.save();
+        res.status(201).render("index")
 
     } catch (error) {
         res.status(400).send(error);
@@ -67,25 +106,34 @@ app.post("/register", async (req,res) => {
 
 // ---------------login form---------------
 
-app.post("/login", async(req,res) => {
+app.post("/login", async (req, res) => {
     try {
         const Username = req.body.Username;
         const password = req.body.password;
 
-        const Usernames = await Register.findOne({Username:Username});
+        const Usernames = await Register.findOne({ Username: Username });
 
         const isMatch = await bcrypt.compare(password, Usernames.password);
-           
+
         const token = await Usernames.generateAuthToken();
-        console.log("the token part: " + token)
-        if(isMatch){
+        console.log("the token part: " + token);
+
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 60000),
+            httpOnly: true,
+            // secure:true
+        });
+
+
+
+        if (isMatch) {
             res.status(201).render("index");
 
-       }else{
-           res.send("Invalid Login Details")
-       }
+        } else {
+            res.send("Invalid Login Details")
+        }
 
-    }catch (error) {
+    } catch (error) {
         res.status(400).send("Invalid Login Details")
     }
 })
@@ -132,7 +180,7 @@ app.post("/login", async(req,res) => {
 // const jwt = require("jsonwebtoken");
 
 // const createToken = async() => {
-    
+
 //     const token = await jwt.sign({_id:"60ffb96306024723a842aac7"}, "mynameisyakhubkhanfromkodad", {
 //         expiresIn: "20 seconds"
 //     });
@@ -147,4 +195,4 @@ app.post("/login", async(req,res) => {
 
 app.listen(port, () => {
     console.log(`server is running at port no ${port}`)
-})   
+})
